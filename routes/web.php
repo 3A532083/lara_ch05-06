06a834036fd748cdb7737cb4a7ab3dd2e4d4c096
3A532083 <luckyday604@gm.student.ncut.edu.tw>
2018-11-11 03:05:18 +0800

    練習3

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('student/{student_no}/score', function ($student_no) {
-    return "學號".$student_no."的".&subject."成績";
+Route::get('student/{student_no}', function ($student_no) {
+    return "學號".$student_no;
+});
+
+Route::get('student/{student_no}/score/{subject?}', function ($student_no, &subject=null) {
+    return "學號".$student_no."的".subject."成績";
 });
