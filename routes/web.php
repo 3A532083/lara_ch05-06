1dd22944ac35cdd8ddf3de7eaca58c042d4e9efc
3A532083 <luckyday604@gm.student.ncut.edu.tw>
2018-11-11 12:56:45 +0800

    練習8

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 */
 
 Route::pattern('student_no','s[0-9]{10}');
+Route::get('/','HomeController@index');
 Route::group(['prefix'=>'student'],function() {
     Route::get('{student_no}', [
         'as'=>'student',
@@ -22,8 +23,8 @@ Route::group(['prefix'=>'student'],function() {
 
     Route::get('{student_no}/score/{subject?}', [
         'as'=>'student.score',
-        'uses'=>function ($student_no,&subject=null){
-            return '學號:' . $student_no . '的' . ((is_null(&subject))? '所有科目:'  &subject.'成績' ;
+        'uses'=>function ($student_no){
+            return '學號:' . $student_no . '的' . '所有科目'  &subject.'成績' ;
     }
 
 ])->where(['subject' => '(chinese| english| math']);
